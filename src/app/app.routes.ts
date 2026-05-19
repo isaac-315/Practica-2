f76e86c28a18bd7ee87cff824d352c81043558bc
isaac-315 <isaacalf.135@gmail.com>
2026-05-19 17:26:30 -0500

    END: Practica 04 - Estilos y Layout con Tailwind completada

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { HomePage } from './features/home/pages/home-page/home-page';
 import { StudentsPage } from './features/student/pages/students-page/students-page';
 import { StudentDetail } from './features/student/pages/students-detail/student-detail';
 import { RouterOutlet } from '@angular/router';
+import { LayoutsPage } from './components/layouts/layouts-page';
 
 export const routes: Routes = [
   { path: '', component: HomePage },
   { path: 'students', component: StudentsPage },
   { path: 'students/:id', component: StudentDetail },
+  { path: 'layouts', component: LayoutsPage },
   { path: '**', redirectTo: '' },
 ];
